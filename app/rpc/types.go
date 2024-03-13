12ae58ba6be59fee4d0fba29709a5f316b7371e2
leftyy <ustyantsev.makar@mail.ru>
2024-03-13 14:08:23 +0300

    make db/rpc models

--- app/rpc/types.go
+++ app/rpc/types.go
@@ -1,1 +1,21 @@
 package rpc
+
+import (
+	"crud/app/models"
+
+	pb "crud/app/proto"
+
+	"github.com/rs/zerolog"
+)
+
+type Server struct {
+	pb.UnimplementedCrudServer
+	PG  RPCIface
+	Log *zerolog.Logger
+}
+
+type RPCIface interface {
+	GetNodeParents(node models.GraphNode) ([]models.GraphNode, error)
+	GetNodePrice(node models.GraphNode) (error, price int32)
+	SetNodePrice(node models.GraphNode) error
+}
